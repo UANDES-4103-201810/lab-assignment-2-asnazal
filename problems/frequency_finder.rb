8289e6737785a1f2bf86a63c6e75c9d0ed612679
asnazal <asnazal@miuandes.cl>
2018-03-20 13:59:13 -0300

    Add problems

---
+++ problems/frequency_finder.rb
@@ -0,0 +1,5 @@
+def find_frequency(sentence, word)
+  sentence=sentence.downcase
+  word=word.downcase
+  sentence.split.count(word)
+end
---
+++ problems/sum_of_cubes.rb
@@ -0,0 +1,11 @@
+def sum_of_cubes(a, b)
+  # Write your code here
+    sum = 0
+    for x in a..b
+       c = 0
+       c = x ** 3 
+       sum += c
+       
+    end
+  return sum
+end
